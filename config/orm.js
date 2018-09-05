12d4561a7ab6d9d5d0cd12efc90e376dd6144f62
CB6 <melissa.j.womack@gmail.com>
2018-09-05 18:38:02 -0500

    changed orm function names, created file to call orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,5 @@
 var connection = require("../config/connection.js");
 
-// * `selectAll()`
-// * `insertOne()`
-// * `updateOne()`
-
 function printQuestionMarks(num) {
     var arr = [];
 
@@ -30,7 +26,7 @@ function objToSql(ob) {
 };
 
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -39,7 +35,7 @@ var orm = {
         cb(result);
       });
     },
-    create: function(table, cols, vals, cb) {
+    insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -59,7 +55,7 @@ var orm = {
         cb(result);
       });
     },
-    update: function(table, objColVals, condition, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
       var queryString = "UPDATE " + table;
   
       queryString += " SET ";
